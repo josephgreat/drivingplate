a944e2235a72e124ea40d71a8567f734941c59e9
Joseph <josephgreat2003@gmail.com>
2022-09-09 00:26:13 +0100

    worked on mobile navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <p class="bg-red-500">Let's see</p>
+    <navbar/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
+<script setup>
+
+import Navbar from './components/Navbar.vue';
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
