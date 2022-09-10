e4ec68f008096b8f38efd9dd9a647cf3d7148ee5
Joseph <josephgreat2003@gmail.com>
2022-09-10 06:49:59 +0100

    added video iframe for parallax

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,9 +1,16 @@
 <template>
-  <section class="jumbotron pb-5 min-h-[60vh]">
+  <section class="jumbotron relative pb-5 min-h-[60vh]">
+    <iframe
+      id="vimeoplayer"
+      src="//player.vimeo.com/video/434167988?api=1&amp;background=1"
+      class="absolute -top-14 left-0 min-w-full min-h-full -z-20"
+    ></iframe>
     <div class="overlay absolute top-0 left-0 bg-[rgba(20,20,20,.4)]"></div>
     <navbar />
-    <div class="h-[50vh] sm:h-[60vh] md:h-[65vh]"></div>
-    <div class="flex justify-end px-4 sm:px-12 md:px-16 lg:px-20 xl:px-[100px] 2xl:px-0 2xl:max-w-[1700px] 2xl:mx-auto">
+    <div class="h-[25vh] sm:h-[50vh] md:h-[55vh] lg:h-[60vh]"></div>
+    <div
+      class="flex justify-end px-4 sm:px-12 md:px-16 lg:px-20 xl:px-[100px] 2xl:px-0 2xl:max-w-[1700px] 2xl:mx-auto"
+    >
       <a
         href="#"
         class="text-white border-2 text-[10px] uppercase font-bold tracking-[.3rem] px-[25px] py-[12px] block border-white hover:bg-white transition duration-3000 ease-linear hover:text-[#191606] group-hover:"
@@ -20,12 +27,8 @@ import Navbar from "./Navbar.vue";
 .jumbotron {
   font-family: "Heebo", Arial, Helvetica, sans-serif;
 }
-.jumbotron {
-  background-image: url("../assets/images/jumbo2.jpg");
-  background-size: cover;
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
+iframe {
   background-color: #7e3a3a;
+  
 }
 </style>
