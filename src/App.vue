2e237a9552df8cd485a7055344c0f8f87286ec19
Joseph <josephgreat2003@gmail.com>
2022-10-12 14:52:47 +0100

    created library page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="min-h-[150vh]">
-    <jumbotron />
-    <page-body/>
+  <div>
+    
+    <library/>
     <footer-section />
   </div>
 </template>
 
 <script setup>
-import Jumbotron from "./components/Jumbotron.vue";
-import PageBody from "./components/PageBody.vue"
+import Library from "./components/pages/Library.vue";
 import FooterSection from './components/FooterSection.vue';
 
 </script>
---
+++ src/components/pages/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <jumbotron />
+        <page-body/>
+    </div>
+</template>
+<script>
+import Jumbotron from '../Jumbotron.vue';
+import PageBody from '../PageBody.vue';
+export default {
+    components: {
+        Jumbotron,
+        PageBody
+    }
+}
+</script>
